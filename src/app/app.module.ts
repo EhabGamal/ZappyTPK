eb0d071b88c4986f91980322cfed0adf7e3058ba
Ehab Gamal <ehab.g.sh@gmail.com>
2017-12-30 21:52:17 +0200

    Added material components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {
+  MatIconModule,
+  MatToolbarModule,
+  MatProgressBarModule,
+  MatCardModule,
+  MatListModule,
+  MatInputModule,
+  MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
