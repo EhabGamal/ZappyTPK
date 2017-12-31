16f6fcde9948aeabecf2975be9381fc73ff6098c
Ehab Gamal <ehab.g.sh@gmail.com>
2017-12-31 09:33:14 +0200

    Added navbar and loading spinner to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TweetsService } from '../services/tweets.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,22 +14,29 @@ export class HomeComponent implements OnInit {
   tweets: Array<Object> = [];
   loading = false;
 
-  constructor( private tweetsService: TweetsService ) { }
+  constructor( private tweetsService: TweetsService, private authService: AuthService ) { }
 
   ngOnInit() {
     this.loadTweets();
   }
 
   loadTweets() {
+    this.loading = true;
     this.tweetsService.get()
     .subscribe(
       (tweets) => {
         console.log(tweets);
         this.tweets = tweets;
+        this.loading = false;
       },
       (error) => {
         console.log(error);
+        this.loading = false;
       }
     );
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
